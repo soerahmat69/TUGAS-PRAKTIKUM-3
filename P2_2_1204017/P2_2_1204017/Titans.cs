b41a2b09e92a7d950e8e84e1b450864c82e41af5
S O E R A H M A T_^ <90734813+soerahmat69@users.noreply.github.com>
2021-11-14 16:01:47 +0700

    LATIHAN

---
+++ P2_2_1204017/P2_2_1204017/Titans.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204017
+{
+   public class Titans : Manusia
+    {
+        protected string kemampuan;
+
+        public Titans(string jenis, string kemampuan) : base(jenis,kemampuan)
+        {
+
+           
+            this.Jenis = jenis;
+            this.Nama = "EREN YEGER";
+            this.kemampuan = kemampuan;
+        }
+
+        public string Kemampuan
+        {
+            get
+            {
+                return kemampuan;
+            }
+            set
+            {
+               kemampuan = value;
+            }
+        }
+    }
+}
